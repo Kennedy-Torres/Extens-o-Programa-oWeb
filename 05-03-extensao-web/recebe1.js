b4201e23aa3f05c1bf7c1d62fb1ba7e3b6c7d65a
Kennedy Rodrigo Torres Gonçalves <128331199+Kennedy-Torres@users.noreply.github.com>
2024-03-05 21:34:45 -0300

    Add files via upload

---
+++ 05-03-extensao-web/recebe1.js
@@ -0,0 +1,7 @@
+var nome = prompt("dg o nome:");
+var sobrenome = prompt("dg o sobrenome:");
+
+console.log("BemVindo "+ nome + "<br> Sobrenome " + sobrenome);
+document.write("BemVindo "+ nome + "<br> Sobrenome " + sobrenome);
+//obs.1: aspas simples consegue interpretar comandos, diferentemente das duplas
+//obs.2: se quiser usar console.log ao invés de document.write, irá apenas mostrar dentro do console/terminal
